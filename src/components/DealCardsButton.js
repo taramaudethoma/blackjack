17123e595428f98f2511006fdf381e8b8c3ed397
Tara Thoma <taramaudethoma@gmail.com>
2023-02-28 14:58:50 -0500

    rough draft

---
+++ src/components/DealCardsButton.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Button } from '@mui/material'
+
+function DealCardsButton() {
+  return (
+    <div><Button className='deal-cards'>Deal Cards</Button></div>
+  )
+}
+
+export default DealCardsButton
